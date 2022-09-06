d01e0df17224987df900766737fa5e05007f14e4
Vladimir <noise123q@mail.ru>
2022-09-06 23:28:06 +0300

    fix bug with margin main container,add block with left side

--- src/main.js
+++ src/main.js
@@ -2,37 +2,40 @@ import "./style.scss"
 
 const toggleMin = document.querySelector(".toggle-min")
 const toggleMax = document.querySelector(".toggle-max")
-const widthField = document.querySelector(".range-controls")
+const widthField = document.querySelector(".range-field")
 const greenBar = document.querySelector(".bar")
-
 
 toggleMax.onmousedown = function (evt) {
     toggleMax.style.transform = "scale(1.1)"
-    
+
+    moveAt(evt.pageX)
+
     function moveAt(pageX) {
+        const fieldRect = widthField.getBoundingClientRect().left;
+
+        toggleMax.style.left = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+        greenBar.style.width = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+
         const widthPaddingContainer = widthField.offsetWidth;
-
-        toggleMax.style.left = pageX - toggleMax.offsetWidth / 2 - evt.offsetX + "px";
-        greenBar.style.width = pageX - toggleMax.offsetWidth / 2 - evt.offsetX + "px";
-
         if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
             greenBar.style.width = widthPaddingContainer + "px"
-        }
+        } 
+        if (+greenBar.style.width.replace("px", "") < 0) {
+            greenBar.style.width = 0 + "px"
+        } 
         if (+toggleMax.style.left.replace("px", "") > widthPaddingContainer) {
             toggleMax.style.left = widthPaddingContainer - toggleMax.offsetWidth + "px" 
         }
+        if (+toggleMax.style.left.replace("px", "") < 0) {
+            toggleMax.style.left = 0 + "px" 
+        }
     }
-    
-    moveAt(evt.pageX)
 
     function onMouseMove (evt) {
         moveAt(evt.pageX)
     }
     
     document.addEventListener("mousemove", onMouseMove)
-
-    // let toggleMaxPositionWhenClick = +toggleMax.style.left.replace("px", "")
-    // greenBar.style.width = `${toggleMaxPositionWhenClick} - ${widthPaddingContainer}`;
 
     document.onmouseup = function () {
         document.removeEventListener('mousemove', onMouseMove);
