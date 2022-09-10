ed8404ffc1f08b599fedeff3d79e6d414f9c2f54
Vladimir <noise123q@mail.ru>
2022-09-10 23:49:01 +0300

    add click for scalebar

--- src/main.js
+++ src/main.js
@@ -4,32 +4,37 @@ const toggleMin = document.querySelector(".toggle-min")
 const toggleMax = document.querySelector(".toggle-max")
 const widthField = document.querySelector(".range-field")
 const greenBar = document.querySelector(".bar")
+const scaleBar = document.querySelector(".scale")
 
-toggleMax.onmousedown = function (evt) {
+function moveAt(pageX) {
+    const fieldRect = widthField.getBoundingClientRect().left;
+
+    toggleMax.style.left = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+    greenBar.style.width = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+
+    const widthPaddingContainer = widthField.offsetWidth;
+    if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
+        greenBar.style.width = widthPaddingContainer + "px"
+    } 
+    if (+greenBar.style.width.replace("px", "") < 0) {
+        greenBar.style.width = 0 + "px"
+    } 
+    if (+toggleMax.style.left.replace("px", "") > widthPaddingContainer) {
+        toggleMax.style.left = widthPaddingContainer - toggleMax.offsetWidth + "px" 
+    }
+    if (+toggleMax.style.left.replace("px", "") < 0) {
+        toggleMax.style.left = 0 + "px" 
+    }
+}
+
+scaleBar.addEventListener("click", function(evt){
+    moveAt(evt.pageX)
+})
+
+toggleMax.onmousedown = function(evt) {
     toggleMax.style.transform = "scale(1.1)"
 
     moveAt(evt.pageX)
-
-    function moveAt(pageX) {
-        const fieldRect = widthField.getBoundingClientRect().left;
-
-        toggleMax.style.left = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
-        greenBar.style.width = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
-
-        const widthPaddingContainer = widthField.offsetWidth;
-        if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
-            greenBar.style.width = widthPaddingContainer + "px"
-        } 
-        if (+greenBar.style.width.replace("px", "") < 0) {
-            greenBar.style.width = 0 + "px"
-        } 
-        if (+toggleMax.style.left.replace("px", "") > widthPaddingContainer) {
-            toggleMax.style.left = widthPaddingContainer - toggleMax.offsetWidth + "px" 
-        }
-        if (+toggleMax.style.left.replace("px", "") < 0) {
-            toggleMax.style.left = 0 + "px" 
-        }
-    }
 
     function onMouseMove (evt) {
         moveAt(evt.pageX)
