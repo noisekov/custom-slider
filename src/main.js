2a2c5fc51f3e4363f81a4936011aa8d760533b8e
Vladimir <noise123q@mail.ru>
2022-09-12 22:51:16 +0300

    add options when can add second toggle

--- src/main.js
+++ src/main.js
@@ -2,9 +2,58 @@ import "./style.scss"
 
 const toggleMin = document.querySelector(".toggle-min")
 const toggleMax = document.querySelector(".toggle-max")
-const widthField = document.querySelector(".range-field")
+const widthField = document.querySelector(".field")
 const greenBar = document.querySelector(".bar")
 const scaleBar = document.querySelector(".scale")
+const inputToggle = document.querySelector("#toggleTwo")
+
+//add toggle
+inputToggle.addEventListener("click", function(){
+    toggleMin.classList.toggle("active")
+
+    // move greenBar and toggleMin
+    // function moveAtMin(pageX) {
+    //     const fieldRect = widthField.getBoundingClientRect().left;
+
+    //     toggleMin.style.left = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+    //     greenBar.style.width = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+
+    //     const widthPaddingContainer = widthField.offsetWidth;
+    //     if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
+    //         greenBar.style.width = widthPaddingContainer + "px"
+    //     } 
+    //     if (+greenBar.style.width.replace("px", "") < 0) {
+    //         greenBar.style.width = 0 + "px"
+    //     } 
+    //     if (+toggleMax.style.left.replace("px", "") > widthPaddingContainer) {
+    //         toggleMax.style.left = widthPaddingContainer - toggleMax.offsetWidth + "px" 
+    //     }
+    //     if (+toggleMax.style.left.replace("px", "") < 0) {
+    //         toggleMax.style.left = 0 + "px" 
+    //     }
+    // }
+
+    // toggleMin.onmousedown = function(evt) {
+    //     moveAtMin(evt.pageX)
+
+    //     function onMouseMove (evt) {
+    //         moveAtMin(evt.pageX)
+    //     }
+        
+    //     document.addEventListener("mousemove", onMouseMove)
+
+    //     document.onmouseup = function () {
+    //         document.removeEventListener('mousemove', onMouseMove);
+    //         document.onmouseup = null;
+    //     }
+
+    //     toggleMin.ondragstart = function () {
+    //         return false
+    //     } 
+    // }
+})
+
+
 
 //move greenBar and toggleMax
 function moveAt(pageX) {
@@ -28,28 +77,7 @@ function moveAt(pageX) {
     }
 }
 
-//move greenBar and toggleMin
-// function moveAtMin(pageX) {
-//     const fieldRect = widthField.getBoundingClientRect().left;
-
-//     toggleMin.style.left = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
-//     greenBar.style.width = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
-
-//     const widthPaddingContainer = widthField.offsetWidth;
-//     if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
-//         greenBar.style.width = widthPaddingContainer + "px"
-//     } 
-//     if (+greenBar.style.width.replace("px", "") < 0) {
-//         greenBar.style.width = 0 + "px"
-//     } 
-//     if (+toggleMax.style.left.replace("px", "") > widthPaddingContainer) {
-//         toggleMax.style.left = widthPaddingContainer - toggleMax.offsetWidth + "px" 
-//     }
-//     if (+toggleMax.style.left.replace("px", "") < 0) {
-//         toggleMax.style.left = 0 + "px" 
-//     }
-// }
-
+//move touch
 function moveTouch(clientX) {
     const fieldRect = widthField.getBoundingClientRect().left;
     toggleMax.style.left = clientX - fieldRect - toggleMax.offsetWidth / 2 + "px";
@@ -108,25 +136,6 @@ toggleMax.onmousedown = function(evt) {
         return false
     } 
 }
-
-// toggleMin.onmousedown = function(evt) {
-//     moveAtMin(evt.pageX)
-
-//     function onMouseMove (evt) {
-//         moveAtMin(evt.pageX)
-//     }
-    
-//     document.addEventListener("mousemove", onMouseMove)
-
-//     document.onmouseup = function () {
-//         document.removeEventListener('mousemove', onMouseMove);
-//         document.onmouseup = null;
-//     }
-
-//     toggleMin.ondragstart = function () {
-//         return false
-//     } 
-// }
 
 //touchmove it code doesnt work
 toggleMax.ontouchstart = function(evt) {
