fe0c847696d804e13f9a446f60da43a7f69413fb
Vladimir <noise123q@mail.ru>
2022-11-26 17:14:09 +0300

    add webpack

--- src/main.js
+++ src/main.js
@@ -1,94 +1,95 @@
 import "./style.scss"
 
-const toggleMin = document.querySelector(".toggle-min")
+// const toggleMin = document.querySelector(".toggle-min")
 const toggleMax = document.querySelector(".toggle-max")
 
 const widthField = document.querySelector(".field")
 const greenBar = document.querySelector(".bar")
 const scaleBar = document.querySelector(".scale")
 
-const inputToggle = document.querySelector("#toggleTwo")
 
-//min and max value input
-const inputMinValue = document.querySelector("#minValue")
-const inputMaxValue = document.querySelector("#maxValue")
+// const inputToggle = document.querySelector("#toggleTwo")
 
-//значения минимимум и максимум 
-const barMinValue = document.querySelector(".progress-bar-min-value")
-const barMaxValue = document.querySelector(".progress-bar-max-value")
+// //min and max value input
+// const inputMinValue = document.querySelector("#minValue")
+// const inputMaxValue = document.querySelector("#maxValue")
+
+// //значения минимимум и максимум 
+// const barMinValue = document.querySelector(".progress-bar-min-value")
+// const barMaxValue = document.querySelector(".progress-bar-max-value")
 
 //поле ввода значения Минимум
-barMinValue.innerText = inputMinValue.value
-inputMinValue.addEventListener("input", function() {
-    if (+inputMinValue.value < +inputMaxValue.value) {
-        barMinValue.innerText = inputMinValue.value
-    }
-})
+// barMinValue.innerText = inputMinValue.value
+// inputMinValue.addEventListener("input", function() {
+//     if (+inputMinValue.value < +inputMaxValue.value) {
+//         barMinValue.innerText = inputMinValue.value
+//     }
+// })
 
 //поле ввода значения Максимум
-barMaxValue.innerText = inputMaxValue.value
-inputMaxValue.addEventListener("input", function() {
-    barMaxValue.innerText = inputMaxValue.value
-})
+// barMaxValue.innerText = inputMaxValue.value
+// inputMaxValue.addEventListener("input", function() {
+//     barMaxValue.innerText = inputMaxValue.value
+// })
 
 //шкала со значением минимум
-barMinValue.addEventListener("click", function() {
-    moveAt(0)
-    greenBar.style.width = 0
-})
+// barMinValue.addEventListener("click", function() {
+//     moveAt(0)
+//     greenBar.style.width = 0
+// })
 
 //шкала со значением максимум
-barMaxValue.addEventListener("click", function() {
-    toggleMax.style.transform = `translateX(${scaleBar.offsetWidth - toggleMax.offsetWidth + "px"})`;
-    greenBar.style.width = scaleBar.offsetWidth + "px";
-})
+// barMaxValue.addEventListener("click", function() {
+//     toggleMax.style.transform = `translateX(${scaleBar.offsetWidth - toggleMax.offsetWidth + "px"})`;
+//     greenBar.style.width = scaleBar.offsetWidth + "px";
+// })
 
 //add second toggle
-inputToggle.addEventListener("click", function() {
-    toggleMin.classList.toggle("active")
+// inputToggle.addEventListener("click", function() {
+//     toggleMin.classList.toggle("active")
 
-    // move greenBar and toggleMin
-    // function moveAtMin(pageX) {
-    //     const fieldRect = widthField.getBoundingClientRect().left;
+//     // move greenBar and toggleMin
+//     // function moveAtMin(pageX) {
+//     //     const fieldRect = widthField.getBoundingClientRect().left;
 
-    //     toggleMin.style.left = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
-    //     greenBar.style.width = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+//     //     toggleMin.style.left = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+//     //     greenBar.style.width = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
 
-    //     const widthPaddingContainer = widthField.offsetWidth;
-    //     if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
-    //         greenBar.style.width = widthPaddingContainer + "px"
-    //     } 
-    //     if (+greenBar.style.width.replace("px", "") < 0) {
-    //         greenBar.style.width = 0 + "px"
-    //     } 
-    //     if (+toggleMax.style.left.replace("px", "") > widthPaddingContainer) {
-    //         toggleMax.style.left = widthPaddingContainer - toggleMax.offsetWidth + "px" 
-    //     }
-    //     if (+toggleMax.style.left.replace("px", "") < 0) {
-    //         toggleMax.style.left = 0 + "px" 
-    //     }
-    // }
+//     //     const widthPaddingContainer = widthField.offsetWidth;
+//     //     if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
+//     //         greenBar.style.width = widthPaddingContainer + "px"
+//     //     } 
+//     //     if (+greenBar.style.width.replace("px", "") < 0) {
+//     //         greenBar.style.width = 0 + "px"
+//     //     } 
+//     //     if (+toggleMax.style.left.replace("px", "") > widthPaddingContainer) {
+//     //         toggleMax.style.left = widthPaddingContainer - toggleMax.offsetWidth + "px" 
+//     //     }
+//     //     if (+toggleMax.style.left.replace("px", "") < 0) {
+//     //         toggleMax.style.left = 0 + "px" 
+//     //     }
+//     // }
 
-    // toggleMin.onmousedown = function(evt) {
-    //     moveAtMin(evt.pageX)
+//     // toggleMin.onmousedown = function(evt) {
+//     //     moveAtMin(evt.pageX)
 
-    //     function onMouseMove (evt) {
-    //         moveAtMin(evt.pageX)
-    //     }
+//     //     function onMouseMove (evt) {
+//     //         moveAtMin(evt.pageX)
+//     //     }
         
-    //     document.addEventListener("mousemove", onMouseMove)
+//     //     document.addEventListener("mousemove", onMouseMove)
 
-    //     document.onmouseup = function () {
-    //         document.removeEventListener('mousemove', onMouseMove);
-    //         document.onmouseup = null;
-    //     }
+//     //     document.onmouseup = function () {
+//     //         document.removeEventListener('mousemove', onMouseMove);
+//     //         document.onmouseup = null;
+//     //     }
 
-    //     toggleMin.ondragstart = function () {
-    //         return false
-    //     } 
-    // }
+//     //     toggleMin.ondragstart = function () {
+//     //         return false
+//     //     } 
+//     // }
 
-})
+// })
 
 //move greenBar and toggleMax
 function moveAt(pageX) {
@@ -113,26 +114,26 @@ function moveAt(pageX) {
 }
 
 //move touch
-function moveTouch(clientX) {
-    const fieldRect = widthField.getBoundingClientRect().left;
+// function moveTouch(clientX) {
+//     const fieldRect = widthField.getBoundingClientRect().left;
     
-    toggleMax.style.transform = `translateX(${clientX - fieldRect - toggleMax.offsetWidth / 2 + "px"})`;
-    greenBar.style.width = clientX - fieldRect - toggleMax.offsetWidth / 2 + "px";
+//     toggleMax.style.transform = `translateX(${clientX - fieldRect - toggleMax.offsetWidth / 2 + "px"})`;
+//     greenBar.style.width = clientX - fieldRect - toggleMax.offsetWidth / 2 + "px";
 
-    const widthPaddingContainer = widthField.offsetWidth;
-    if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
-        greenBar.style.width = widthPaddingContainer + "px"
-    } 
-    if (+greenBar.style.width.replace("px", "") < 0) {
-        greenBar.style.width = 0 + "px"
-    } 
-    if (+toggleMax.style.transform.replace("translateX(", "").replace('px)','') > widthPaddingContainer) {
-        toggleMax.style.transform = `translateX(${widthPaddingContainer - toggleMax.offsetWidth + "px"})`; 
-    }
-    if (+toggleMax.style.transform.replace("translateX(", "").replace('px)','') < 0) {
-        toggleMax.style.transform = `translateX(${0 + "px"})`; 
-    }
-}
+//     const widthPaddingContainer = widthField.offsetWidth;
+//     if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
+//         greenBar.style.width = widthPaddingContainer + "px"
+//     } 
+//     if (+greenBar.style.width.replace("px", "") < 0) {
+//         greenBar.style.width = 0 + "px"
+//     } 
+//     if (+toggleMax.style.transform.replace("translateX(", "").replace('px)','') > widthPaddingContainer) {
+//         toggleMax.style.transform = `translateX(${widthPaddingContainer - toggleMax.offsetWidth + "px"})`; 
+//     }
+//     if (+toggleMax.style.transform.replace("translateX(", "").replace('px)','') < 0) {
+//         toggleMax.style.transform = `translateX(${0 + "px"})`; 
+//     }
+// }
 
 //click to scaleBar
 scaleBar.onmousedown = function(evt) {
@@ -163,21 +164,21 @@ toggleMax.onmousedown = function(evt) {
 }
 
 //touchmove it code doesnt work
-toggleMax.ontouchstart = function(evt) {
-    moveTouch(evt.targetTouches[0].clientX)
+// toggleMax.ontouchstart = function(evt) {
+//     moveTouch(evt.targetTouches[0].clientX)
 
-    function onTouchMove (evt) {
-        moveTouch(evt.targetTouches[0].clientX)
-    }
+//     function onTouchMove (evt) {
+//         moveTouch(evt.targetTouches[0].clientX)
+//     }
     
-    document.addEventListener("touchmove", onTouchMove)
+//     document.addEventListener("touchmove", onTouchMove)
 
-    document.ontouchend =  function (){  
-        document.removeEventListener('touchmove', onTouchMove);
-        document.ontouchend = null;
-    }
+//     document.ontouchend =  function (){  
+//         document.removeEventListener('touchmove', onTouchMove);
+//         document.ontouchend = null;
+//     }
 
-    toggleMax.ondragstart = function () {
-        return false
-    } 
-}
+//     toggleMax.ondragstart = function () {
+//         return false
+//     } 
+// }
