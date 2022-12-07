120d32e04df186a0e98177888abd6707a035109a
Vladimir <noise123q@mail.ru>
2022-12-07 20:49:33 +0300

    fix: delete commented code

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import "./style.scss"
 // const toggleMin = document.querySelector(".toggle-min")
 const toggleMax = document.querySelector(".toggle-max")
 
-// const widthField = document.querySelector(".field")
-// const greenBar = document.querySelector(".bar")
-// const scaleBar = document.querySelector(".scale")
-
 class Toggle {
 
     constructor (toggle) {
@@ -88,7 +84,7 @@ class Toggle {
 
         if (+this.$greenBar.style.width.replace("px", "") > widthPaddingContainer) {
             this.$greenBar.style.width = widthPaddingContainer + "px";
-        } 
+        }
         if (+this.$toggle.style.transform.replace("translateX(", "").replace('px)','') > widthPaddingContainer) {
             this.$toggle.style.transform = `translateX(${widthPaddingContainer - this.$toggle.offsetWidth + "px"})`; 
         }
@@ -99,85 +95,4 @@ class Toggle {
 
 }
 
-new Toggle(toggleMax)
-
-// //mousemove
-// toggleMax.onmousedown = function(evt) {
-//     moveAt(evt.pageX)
-//     document.addEventListener("mousemove", onMouseMove)
-
-//     document.onmouseup = function () {
-//         document.removeEventListener('mousemove', onMouseMove);
-//         document.onmouseup = null;
-//     }
-//     toggleMax.ondragstart = () => false;
-// }
-
-// // //move greenBar and toggleMax
-// function moveAt(pageX) {
-//     const fieldRect = widthField.getBoundingClientRect().left;
-
-//     toggleMax.style.transform = `translateX(${pageX - fieldRect - toggleMax.offsetWidth / 2 + "px"})`;
-//     greenBar.style.width = pageX - fieldRect - toggleMax.offsetWidth / 2 + "px";
-
-//     const widthPaddingContainer = widthField.offsetWidth;
-//     if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
-//         greenBar.style.width = widthPaddingContainer + "px"
-//     } 
-//     if (+toggleMax.style.transform.replace("translateX(", "").replace('px)','') > widthPaddingContainer) {
-//         toggleMax.style.transform = `translateX(${widthPaddingContainer - toggleMax.offsetWidth + "px"})`; 
-//     }
-//     if (+toggleMax.style.transform.replace("translateX(", "").replace('px)','') < 0) {
-//         toggleMax.style.transform = `translateX(${0 + "px"})`;
-//     }
-// }
-// //click to scaleBar
-// scaleBar.onmousedown = function(evt) {
-//     moveAt(evt.pageX)
-//     scaleBar.ondragstart = () => false;
-// }
-
-// function onMouseMove (evt) {
-//     moveAt(evt.pageX)
-// }
-
-// // move touch
-// // function moveTouch(clientX) {
-// //     const fieldRect = widthField.getBoundingClientRect().left;
-    
-// //     toggleMax.style.transform = `translateX(${clientX - fieldRect - toggleMax.offsetWidth / 2 + "px"})`;
-// //     greenBar.style.width = clientX - fieldRect - toggleMax.offsetWidth / 2 + "px";
-
-// //     const widthPaddingContainer = widthField.offsetWidth;
-// //     if (+greenBar.style.width.replace("px", "") > widthPaddingContainer) {
-// //         greenBar.style.width = widthPaddingContainer + "px";
-// //     } 
-// //     if (+greenBar.style.width.replace("px", "") < 0) {
-// //         greenBar.style.width = 0 + "px";
-// //     } 
-// //     if (+toggleMax.style.transform.replace("translateX(", "").replace('px)','') > widthPaddingContainer) {
-// //         toggleMax.style.transform = `translateX(${widthPaddingContainer - toggleMax.offsetWidth + "px"})`; 
-// //     }
-// //     if (+toggleMax.style.transform.replace("translateX(", "").replace('px)','') < 0) {
-// //         toggleMax.style.transform = `translateX(${0 + "px"})`; 
-// //     }
-// // }
-
-
-// // touchmove it code doesnt work
-// // toggleMax.ontouchstart = function(evt) {
-// //     moveTouch(evt.targetTouches[0].clientX)
-
-// //     function onTouchMove (evt) {
-// //         moveTouch(evt.targetTouches[0].clientX)
-// //     }
-    
-// //     document.addEventListener("touchmove", onTouchMove)
-
-// //     document.ontouchend =  function (){  
-// //         document.removeEventListener('touchmove', onTouchMove);
-// //         document.ontouchend = null;
-// //     }
-
-// //     toggleMax.ondragstart = () => false;
-// // }
+new Toggle(toggleMax);
